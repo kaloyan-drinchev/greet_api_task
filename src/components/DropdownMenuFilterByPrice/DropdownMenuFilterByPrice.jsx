72d97e8f8c61c8cca176c25039eaf96d90c4b6a0
Kaloyan <kalloyand@gmail.com>
2023-10-16 20:57:38 +0300

    Add when click on arbitrary item to close the menu

--- src/components/DropdownMenuFilterByPrice/DropdownMenuFilterByPrice.jsx
+++ src/components/DropdownMenuFilterByPrice/DropdownMenuFilterByPrice.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
 
-export default function DropdownMenuFilterByPrice({ sortByAsc, sortByDesc }) {
+export default function DropdownMenuFilterByPrice({
+  sortAscOrder,
+  sortDescOrder,
+}) {
   const [toggle, setToggle] = useState(false);
 
   const dropdownMenuToggle = () => {
@@ -19,10 +22,20 @@ export default function DropdownMenuFilterByPrice({ sortByAsc, sortByDesc }) {
         Сортирай по Цена
       </button>
       <div className={`dropdown-menu ${toggle ? "show" : ""}`}>
-        <button className="dropdown-item" onClick={sortByAsc}>
+        <button
+          className="dropdown-item"
+          onClick={() => {
+            sortAscOrder(), setToggle(false);
+          }}
+        >
           От ниска към висока
         </button>
-        <button className="dropdown-item" onClick={sortByDesc}>
+        <button
+          className="dropdown-item"
+          onClick={() => {
+            sortDescOrder(), setToggle(false);
+          }}
+        >
           От висока към ниска
         </button>
       </div>
