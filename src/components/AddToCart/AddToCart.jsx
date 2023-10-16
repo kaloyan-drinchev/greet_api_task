ffa12ee80c51258d2f0b77e8a4a48ef1e73efa36
Kaloyan <kalloyand@gmail.com>
2023-10-16 10:49:17 +0300

    Add AddToCart component

--- src/components/AddToCart/AddToCart.jsx
+++ src/components/AddToCart/AddToCart.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
 export default function AddToCart({ id }) {
   const handleClick = () => {
-    window.location.href = `https://greet.bg/?add-to-cart=${id}`;
+    window.open(`https://greet.bg/?add-to-cart=${id}`, "_target");
   };
 
   return (
     <button
       className="text-center bg-primary text-white rounded"
       onClick={handleClick}
+      target="_blank"
     >
       Add To Cart
     </button>
